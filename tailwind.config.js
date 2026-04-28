8202bc15339e7087d2ad114af56b1a165b0e8a75
Ashwin Raman <ashwiniyer.1691@gmail.com>
2026-04-28 18:45:40 -0500

    chore: document border radius system, confirm error colour standardisation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+/**
+ * Border radius system:
+ *   Cards and modals:       rounded-3xl  (1.5rem)
+ *   Input fields:           rounded-2xl  (1rem)
+ *   Full-width buttons:     rounded-2xl  (1rem)
+ *   Small/inline buttons:   rounded-xl   (0.75rem)
+ *   Pills and tags:         rounded-full
+ *   Item thumbnails grids:  rounded-2xl  (1rem)
+ */
 export default {
   darkMode: false,
   content: ['./index.html', './src/**/*.{js,jsx}'],
